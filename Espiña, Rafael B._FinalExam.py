c74e99df8ecaa8d0324da795671435bf1035c1ac
RafaelEspina <88146669+RafaelEspina@users.noreply.github.com>
2022-05-23 10:58:40 +0800

    Update Espiña, Rafael B._FinalExam.py

--- Espiña, Rafael B._FinalExam.py
+++ Espiña, Rafael B._FinalExam.py
@@ -4,7 +4,7 @@ window = Tk()
 window.title("Find The Least Number")
 window.geometry("400x300+20+10")
 
-#
+# definition to find the smallest number
 def findLeast():
     Numbers = []
     Numbers.append(eval(firstNumber.get()))
@@ -12,35 +12,38 @@ def findLeast():
     Numbers.append(eval(thirdNumber.get()))
     smallestNumber.set(min(Numbers))
 
-lbl1 = Label(window, text = "A Program That Finds The Smallest Number")
-lbl1.grid(row=0, column=1, columnspan=2,sticky=EW)
+# Labels, Input and Output
+Label_1 = Label(window, text = "A Program That Finds The Smallest Number")
+Label_1.grid(row=0, column=1, columnspan=2,sticky=EW)
 
-lbl2 = Label(window,text = "Enter the first number:")
-lbl2.grid(row=1, column = 0,sticky=W)
+Label_2 = Label(window,text = "Enter the first number:")
+Label_2.grid(row=1, column = 0,sticky=W)
 firstNumber = StringVar()
-ent2 = Entry(window,bd=3,textvariable=firstNumber)
-ent2.grid(row=1, column = 1)
+input_2 = Entry(window,bd=3,textvariable=firstNumber)
+input_2.grid(row=1, column = 1)
 
-lbl3 = Label(window,text = "Enter the second number:")
-lbl3.grid(row=2, column=0)
+Label_3 = Label(window,text = "Enter the second number:")
+Label_3.grid(row=2, column=0)
 secondNumber=StringVar()
-ent3 = Entry(window,bd=3,textvariable=secondNumber)
-ent3.grid(row=2,column=1)
+input_3 = Entry(window,bd=3,textvariable=secondNumber)
+input_3.grid(row=2,column=1)
 
-lbl4 = Label(window,text="Enter the third number:")
-lbl4.grid(row=3,column =0, sticky=W)
+Label_4 = Label(window,text="Enter the third number:")
+Label_4.grid(row=3,column =0, sticky=W)
 thirdNumber = StringVar()
-ent4 = Entry(window,bd=3,textvariable=thirdNumber)
-ent4.grid(row=3, column=1)
+input_4 = Entry(window,bd=3,textvariable=thirdNumber)
+input_4.grid(row=3, column=1)
 
-btn1 = Button(window,text = "Find The Smallest Number",command=findLeast)
-btn1.grid(row=4, column = 1)
+#button to start the function
+buttonSmallest = Button(window,text = "Find The Smallest Number",command=findLeast)
+buttonSmallest.grid(row=4, column = 1)
 
-lbl5 = Label(window,text="The Least Number: ")
-lbl5.grid(row=5,column=0,sticky=W)
+#output
+Label_5 = Label(window,text="The Least Number: ")
+Label_5.grid(row=5,column=0,sticky=W)
 smallestNumber = StringVar()
-ent5 = Entry(window,bd=3,state="readonly",textvariable=smallestNumber)
-ent5.grid(row=5,column=1)
+Input_5 = Entry(window,bd=3,state="readonly",textvariable=smallestNumber)
+Input_5.grid(row=5,column=1)
 
 
 mainloop()
